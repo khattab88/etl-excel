207e58407bdf7738f85ae7373c179ad1ef7b8bab
mahmoud.khattab <mahmoud.khattab@MAHMOUD-KHATTAB>
2021-09-15 03:25:08 +0200

    create unit tests for Extractor

---
+++ test/etl/extractors/ageCategoryByVillageExtractor.test.ts
@@ -0,0 +1,9 @@
+import { AgeCategoryByVillageExtractor } from '../../../src/etl/extractors/ageCategoryByVillageExtractor';
+
+test('Constructor_IntializeCorrectSheetIndexAndName_SetCorrectSheetIndexAndName', () => {
+    var extractor = new AgeCategoryByVillageExtractor();
+
+    expect(extractor.sheetIndex).toBe(1);
+    expect(extractor.sheetName).toBe("Sheet1");
+});
+
