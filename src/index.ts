1c1e347552ebc21088cc034b91e5652cd8441347
mahmoud.khattab <mahmoud.khattab@MAHMOUD-KHATTAB>
2021-09-19 04:33:13 +0200

    remove Echo function from package api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import { FileType } from './etl/enums/fileType';
 import { ETL } from './etl';
 
-const Echo = (name: string) => `Hello ${name}`;
-
-export { Echo, ETL, FileType };
+export { ETL, FileType };
